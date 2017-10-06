aa711d1a646b537b7be0a72057bba762c68f0b88
Dominic Bilke <DominicBilke@GMail.com>
2017-10-06 08:22:05 +0200

    GUI für QT

---
+++ server_client_gui/mainwindow.cpp
@@ -0,0 +1,54 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setHostName("localhost");
+    db.setDatabaseName("srv_clnt");
+    db.setUserName("client");
+    db.setPassword("client");
+    bool ok = db.open();
+
+    qDebug() << "Database open? " << ok;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_refreshButton_clicked()
+{
+
+    qDebug() << "SELECT * FROM client";
+
+    QSqlDatabase::database().transaction();
+    QSqlQuery query;
+    query.exec("SELECT * FROM client");
+
+    while (query.next()) {
+           QString ip = query.value(0).toString();
+           QString text = query.value(1).toString();
+           ui->textBrowser->append(ip+": "+text+"\n");
+           qDebug() << ip << ": " << text << "\n";
+       }
+    QSqlDatabase::database().commit();
+}
+
+void MainWindow::on_sendButton_clicked()
+{
+    qDebug() << "INSERT INTO client(IP, MSG) VALUES('" << ui->ipTextEdit->toPlainText() << "', '" << ui->sendTextEdit->toPlainText() << "')";
+
+    QSqlDatabase::database().transaction();
+    QSqlQuery query;
+    query.prepare("INSERT INTO client(IP, MSG) VALUES(':ip', ':msg')");
+    query.bindValue(":ip", ui->ipTextEdit->toPlainText());
+    query.bindValue(":msg", ui->sendTextEdit->toPlainText());
+    query.exec();
+    QSqlDatabase::database().commit();
+}
---
+++ server_client_gui/server_client_gui.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-09-20T11:37:54
+#
+#-------------------------------------------------
+
+QT       += core gui sql
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = server_client_gui
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
